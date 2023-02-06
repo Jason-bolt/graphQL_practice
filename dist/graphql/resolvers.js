e12b1c2f833863caae87f7f9856fb247f8c581ea
Jason-bolt <appiatujason@gmail.com>
2023-02-06 10:22:43 +0000

    New features

--- dist/graphql/resolvers.js
+++ dist/graphql/resolvers.js
@@ -1,92 +1,87 @@
-import Author from "../models/Authors.js";
-import Book from "../models/Books.js";
+import Author from "../models/Authors";
+import Book from "../models/Books";
 const resolvers = {
-  Query: {
-    // Authors Query
-    authors: async () => {
-      return await Author.find();
+    Query: {
+        // Authors Query
+        authors: async () => {
+            return await Author.find();
+        },
+        author: async (_, { id }) => {
+            return await Author.findOne({ _id: id });
+        },
+        // Books Query
+        books: async () => {
+            return await Book.find();
+        },
+        book: async (_, { id }) => {
+            return await Book.findOne({ _id: id });
+        },
     },
-    author: async (_, { id }) => {
-      return await Author.findOne({ _id: id });
+    Book: {
+        author: async ({ author }) => {
+            const author_id = author.toHexString();
+            const bookAuthor = await Author.findOne({ _id: author_id });
+            return bookAuthor;
+        },
     },
-    // Books Query
-    books: async () => {
-      return await Book.find();
+    Mutation: {
+        // Authors Mutations
+        createAuthor: async (_, { input }) => {
+            const { first_name, last_name } = input;
+            const newAuthor = await Author.create({
+                first_name: first_name,
+                last_name: last_name,
+            });
+            return newAuthor;
+        },
+        editAuthor: async (_, { id, input }) => {
+            const { first_name, last_name } = input;
+            const isEdited = await Author.updateOne({ _id: id }, {
+                first_name: first_name,
+                last_name: last_name,
+            });
+            if (isEdited) {
+                return await Author.findOne({ _id: id });
+            }
+        },
+        deleteAuthor: async (_, { id }) => {
+            const isDeleted = await Author.deleteOne({ _id: id });
+            if (isDeleted) {
+                await Book.deleteMany({ author: id });
+                return true;
+            }
+            return false;
+        },
+        // Books Mutations
+        createBook: async (_, { input }) => {
+            const { title, pages, author } = input;
+            const newBook = await Book.create({
+                title: title,
+                pages: pages,
+                author: author,
+            });
+            return newBook;
+        },
+        editBook: async (_, { id, input }) => {
+            const { title, pages, author } = input;
+            const isEdited = await Book.updateOne({ _id: id }, {
+                title: title,
+                pages: pages,
+                author: author,
+            });
+            if (isEdited) {
+                return Book.findOne({ _id: id });
+            }
+        },
+        deleteBook: async (_, { id }) => {
+            const isDeleted = await Book.deleteOne({ _id: id });
+            if (isDeleted) {
+                return true;
+            }
+            else {
+                return false;
+            }
+        },
     },
-    book: async (_, { id }) => {
-      return await Book.findOne({ _id: id });
-    },
-  },
-  Book: {
-    author: async ({ author }) => {
-      const author_id = author.toHexString();
-      const bookAuthor = await Author.findOne({ _id: author_id });
-      return bookAuthor;
-    },
-  },
-  Mutation: {
-    // Authors Mutations
-    createAuthor: async (_, { input }) => {
-      const { first_name, last_name } = input;
-      const newAuthor = await Author.create({
-        first_name: first_name,
-        last_name: last_name,
-      });
-      return newAuthor;
-    },
-    editAuthor: async (_, { id, input }) => {
-      const { first_name, last_name } = input;
-      const isEdited = await Author.updateOne(
-        { _id: id },
-        {
-          first_name: first_name,
-          last_name: last_name,
-        }
-      );
-      if (isEdited) {
-        return await Author.findOne({ _id: id });
-      }
-    },
-    deleteAuthor: async (_, { id }) => {
-      const isDeleted = await Author.deleteOne({ _id: id });
-      if (isDeleted) {
-        await Book.deleteMany({ author: id });
-        return true;
-      }
-      return false;
-    },
-    // Books Mutations
-    createBook: async (_, { input }) => {
-      const { title, pages, author } = input;
-      const newBook = await Book.create({
-        title: title,
-        pages: pages,
-        author: author,
-      });
-      return newBook;
-    },
-    editBook: async (_, { id, input }) => {
-      const { title, pages, author } = input;
-      const isEdited = await Book.updateOne(
-        { _id: id },
-        {
-          title: title,
-          pages: pages,
-          author: author,
-        }
-      );
-      if (isEdited) {
-        return Book.findOne({ _id: id });
-      }
-    },
-    deleteBook: async (_, { id }) => {
-      const isDeleted = await Book.deleteOne({ _id: id });
-      if (isDeleted) {
-        return true;
-      } else {
-        return false;
-      }
-    },
-  },
 };
 export default resolvers;
--- dist/server.js
+++ dist/server.js
@@ -3,25 +3,25 @@ import { startStandaloneServer } from "@apollo/server/standalone";
 import { connect } from "mongoose";
 import dotenv from "dotenv";
 dotenv.config();
-import resolvers from "./graphql/resolvers.js";
-import typeDefs from "./graphql/typeDefs.js";
+import resolvers from "./graphql/resolvers";
+import typeDefs from "./graphql/typeDefs";
 class Server {
-  static async connectDB(mongoURI) {
-    return await connect(mongoURI);
-  }
-  static async connectApollo(typeDefs, resolvers) {
-    const server = new ApolloServer({ typeDefs, resolvers });
-    const { url } = await startStandaloneServer(server, {
-      listen: { port: 4000 },
-    });
-    return url;
-  }
+    static async connectDB(mongoURI) {
+        return await connect(mongoURI);
+    }
+    static async connectApollo(typeDefs, resolvers) {
+        const server = new ApolloServer({ typeDefs, resolvers });
+        const { url } = await startStandaloneServer(server, {
+            listen: { port: 4000 },
+        });
+        return url;
+    }
 }
 const makeDBAndApolloConnection = async () => {
-  const mongoURI = process.env.mongoURI;
-  if (await Server.connectDB(mongoURI)) {
-    const url = await Server.connectApollo(typeDefs, resolvers);
-    console.log(url);
-  }
+    const mongoURI = process.env.mongoURI;
+    if (await Server.connectDB(mongoURI)) {
+        const url = await Server.connectApollo(typeDefs, resolvers);
+        console.log(url);
+    }
 };
 makeDBAndApolloConnection();
